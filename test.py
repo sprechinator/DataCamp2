ac624edcab9d9e4dafb46a9ecb288c3ae5b2493e
Bryan C Sprecher <bsprech@Farva.local>
2017-09-08 18:30:01 -0500

    Initial commit

---
+++ test.py
@@ -0,0 +1,8 @@
+import numpy as np
+#Request and print the `.feature_importances_` attribute
+print(my_tree_two.feature_importances_)
+print(my_forest.feature_importances_)
+
+#Compute and print the mean accuracy score for both models
+print(my_tree_two.score(features_two, target))
+print(my_forest.score(features_forest, target))
---
+++ test2.py
@@ -0,0 +1,3 @@
+import numpy as np
+x = [19, 5, 12, 4, 0, 11, 1]
+print(x[1:4])
